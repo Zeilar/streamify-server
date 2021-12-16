e5abdb07b1ccaecdbbb1fe49d291e327e1cd7179
Philip <philip_angelin@hotmail.com>
2021-12-16 19:48:26 +0100

    Fixed import

--- src/server/modules/auth/auth.module.ts
+++ src/server/modules/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
-import { UserService } from "../user/user.service";
+import { UserModule } from "../user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { LocalStrategy } from "./local.strategy";
 
 @Module({
-    imports: [UserService, PassportModule],
+    imports: [UserModule, PassportModule],
     controllers: [AuthController],
     providers: [AuthService, LocalStrategy],
 })
