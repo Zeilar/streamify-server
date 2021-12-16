a9798eb162aa8415f5b7135d3b28a752f3cbfcda
Philip <philip_angelin@hotmail.com>
2021-12-16 01:32:19 +0100

    Fixed main page component

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,5 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App() {
-    return <ChakraProvider>App</ChakraProvider>;
+export default function App({ Component, pageProps }: any) {
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
