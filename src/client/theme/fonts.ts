56f1e282afe6a3815350156d199566640d9ea350
Philip <philip_angelin@hotmail.com>
2021-12-21 18:27:27 +0100

    Fonts

--- src/client/theme/fonts.ts
+++ src/client/theme/fonts.ts
@@ -1,4 +1,12 @@
-import "@fontsource/open-sans";
+// Open Sans
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/open-sans/800.css";
+
+// Poppins
 import "@fontsource/poppins/100.css";
 import "@fontsource/poppins/200.css";
 import "@fontsource/poppins/300.css";
