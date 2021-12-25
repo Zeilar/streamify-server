90ad5b3d1aa152fb7d325a5dfbdfafa3b7dc6310
Philip <philip_angelin@hotmail.com>
2021-12-25 20:34:54 +0100

    Og tags

--- src/client/pages/video/[id]/index.tsx
+++ src/client/pages/video/[id]/index.tsx
@@ -1,22 +1,34 @@
 import { GetServerSidePropsContext } from "next";
-import { useRouter } from "next/router";
 import axios from "axios";
 import { Video } from "../../../@types/video";
+import Head from "next/head";
 
 interface VideoData {
-    video: Video;
-    videoUrl: string;
+    video?: Video;
+    videoUrl?: string;
 }
 
 export default function SingleVideo({ video, videoUrl }: VideoData) {
     console.log("hello world", video, videoUrl);
-    return <div>Video</div>;
+    return (
+        <div>
+            <Head>
+                <meta property="og:title" content={video.title} />
+                <meta property="og:type" content="video.other" />
+                <meta property="og:video" content={videoUrl} />
+                <meta property="og:video:secure_url" content={videoUrl} />
+                <meta property="og:video:type" content="video/mp4" />
+                <meta property="og:video:width" content="1280" />
+                <meta property="og:video:height" content="720" />
+            </Head>
+        </div>
+    );
 }
 
 export async function getServerSideProps(
     context: GetServerSidePropsContext<{ id: string }>
 ) {
-    let data: VideoData | {} = {};
+    let data: VideoData = {};
     try {
         const response = await axios.get<VideoData>(
             `${process.env.API_BASE_URL}/video/${context.params.id}`
